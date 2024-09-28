73f2b491fe4e0f510d3f1bb962d7f32950fc740b
Tomás <arnotomas1@gmail.com>
2024-09-28 11:59:34 -0300

    paths fixed (#3)

--- src/utils/env-action.util.ts
+++ src/utils/env-action.util.ts
@@ -1,7 +1,7 @@
-import args from "@utils/args.util";
+import args from "./args.util";
 
 if (args.env == "development") {
-  require("@utils/load-env.util");
+  require("./load-env.util");
 } else {
   require("module-alias/register");
 }
