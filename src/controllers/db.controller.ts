40c505f64a2bd1fbfdadf28e229897f23e75cc91
Tomas <arnotomas1@gmail.com>
2024-09-30 08:30:27 -0300

    sync endpoint working

---
+++ src/controllers/db.controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+import sequelize from "src/database/connect";
+
+class DbController {
+  static async sync(req: Request, res: Response, next: NextFunction) {
+    try {
+      sequelize.sync();
+
+      res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default DbController;
