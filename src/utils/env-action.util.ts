e5cb36e19d276eb16299c9a37177db41d4a395ce
Tomas <arnotomas1@gmail.com>
2024-10-31 11:14:33 -0300

    path errors fixed

--- src/utils/env-action.util.ts
+++ src/utils/env-action.util.ts
@@ -1,9 +1,11 @@
 import args from "./args.util";
 
 (() => {
-  if (args.env == "development") {
+  if (args.env == "development" || args.env == "test") {
     require("./load-env.util");
-  } else {
+  }
+
+  if (args.env == "production" || args.env == "test") {
     require("module-alias/register");
   }
 })();
